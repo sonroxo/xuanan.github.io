b4944f5ba445cab60acdda89f858188570f4f6d2
root <root@XuanAnPC>
2023-04-01 17:02:51 +0700

    update to personal website

---
+++ src/Containers/CurriculumVitae.js
@@ -0,0 +1,34 @@
+export default function CurriculumVitae() {
+  return (
+    <>
+      {/* Wrap personal info */}
+      <div className="p-2">
+        <h1 className="mt-8 text-center uppercase p-2">My Curriculum Vitae</h1>
+        <div className="h-8 " name="break" />
+        <div className="w-[350px] m-auto ">
+          <h6 className="p-2 font-bold"> Nguyễn Trường Xuân An </h6>
+          <p className="p-2">
+            <strong>DOB:</strong> 1992
+          </p>
+          <p className="p-2">
+            <strong>Address:</strong> Hanoi
+          </p>
+          <p className="p-2">
+            <strong>Email:</strong> nguyentruongxuananhn@gmail.com
+          </p>
+          <p className="p-2">
+            <strong>Phone:</strong> 0919.871.216
+          </p>
+        </div>
+      </div>
+
+      {/* Personal content details in timeline */}
+      <div className="h-8 " name="break" />
+      <h2 className="text-center uppercase ">Course of Life</h2>
+      <div className="p-2">
+
+      </div>
+    </>
+  )
+
+}
