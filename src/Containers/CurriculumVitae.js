0e04f54a647172f01c397326a48d04fb41b0ae86
root <root@XuanAnPC>
2023-04-02 00:08:50 +0700

    big update

--- src/Containers/CurriculumVitae.js
+++ src/Containers/CurriculumVitae.js
@@ -1,12 +1,12 @@
 export default function CurriculumVitae() {
   return (
-    <>
+    <div className="sm:w-full md:w-6/12 m-auto">
       {/* Wrap personal info */}
       <div className="p-2">
         <h1 className="mt-8 text-center uppercase p-2">My Curriculum Vitae</h1>
         <div className="h-8 " name="break" />
         <div className="w-[350px] m-auto ">
-          <h6 className="p-2 font-bold"> Nguyễn Trường Xuân An </h6>
+          <h6 className="uppercase p-2 font-bold"> Nguyễn Trường Xuân An </h6>
           <p className="p-2">
             <strong>DOB:</strong> 1992
           </p>
@@ -25,10 +25,130 @@ export default function CurriculumVitae() {
       {/* Personal content details in timeline */}
       <div className="h-8 " name="break" />
       <h2 className="text-center uppercase ">Course of Life</h2>
-      <div className="p-2">
+      <div className="h-8 " name="break" />
 
-      </div>
-    </>
+      <h3>Skills</h3>
+      <ul>
+        <li className="italic">Coding: Reactjs, Redux, Javascript, NodeJS, ASP.Net C#, Microsoft SQL.</li>
+        <li className="italic">Miscellenous: Bootstrap, Tailwindcss, CSS, Sass, Git, Firebase, Netlify, Ajax, Fontawesome, Restful API, Nginx.</li>
+        <li className="italic">Management: BSC, 3Ps, OKRs, Accounting reports.</li>
+        <li className="italic">Communication & Leadership: Coaching, GROW model.</li>
+      </ul>
+
+      <div className="h-8 " name="break" />
+
+      <h3>Experience</h3>
+      <h6>Manager</h6>
+      <p>Roxo Co., Ltd, Hanoi (2017-2021)</p>
+      <ul>
+        <li>- Using Balance scorecard and strategic management tool to penetrate the market.</li>
+        <li>- Coordinate departments to amplify productivity with cost deduction in operation.</li>
+        <li>- Develop new market through holistic marketing and campaign (advertising, digital marketing, direct sales, public relations, promotion).</li>
+        <li>- Handle finance cashflow, P&L, balance sheet. Analyze to twist operation performance as well as proper investment for marketing campaign, human recruit, and factory expand.</li>
+        <li>- Build 3P Compensation by using excel tool and salary market research.</li>
+        <li>- Design and build company’s brand.</li>
+      </ul>
+      <h6>Business Consultant - Internship</h6>
+      <p>LSBDC - Louisiana Small Business Development Center (Q4-2016)</p>
+      <ul>
+        <li>- Advisory for small businesses in local area.</li>
+        <li>- Help small businesses in financial reports and financial projection for raising funds from government.</li>
+        <li>- Work in diversity workplace.</li>
+      </ul>
+      <h6>Graduate Assistant for Professor</h6>
+      <p>McNeese State University - Louisiana, USA (2016)</p>
+      <ul>
+        - Do market research and survey for Dr.
+        - Assist Dr. on tasks and schedule.
+      </ul>
+      <h6>Service Staff at Library</h6>
+      <p>McNeese State University - Louisiana, USA (2016)</p>
+      <ul>
+        <li>- Arrange and clean books to shelves</li>
+        <li>- Lab service admin. Deal with students’ feedback and support them using machines.</li>
+      </ul>
+      <h6>Founder</h6>
+      <p>Highsky Coffee, Hanoi (2013-2014)</p>
+      <ul>
+        <li>- Business model is similar with The Coffee Beans and Starbuck.</li>
+        <li>Manage over 20 employees for the shop, full-time and part-time.</li>
+        <li>Product-testing, design to fit customer’s target.</li>
+        <li>Process payrulls, payslips and accounting for the shop.</li>
+        <li>Deal with customers’ feedback.</li>
+        <li>Marketing for the shop.</li>
+        <li>Break-even within a year. Stop to apply graduate schoul.</li>
+      </ul>
+
+      <div className="h-8 " name="break" />
+      <h3>Education</h3>
+      <h6>MBA Business Administration</h6>
+      <ul>
+        <li>McNeese State University, Lake Charles, Louisiana, USA.</li>
+        <li>Graduate at 12/2016.</li>
+      </ul>
+      <h6>Bachelor Business Administration</h6>
+      <ul>
+        <li>East Asia Institute of Management from Singapore – University of Cardiff from England.</li>
+        <li>Graduate at 7/2013.</li>
+      </ul>
+      <h6>Hanoi Amsterdam Highschool</h6>
+      <ul>
+        <li>Graduate at 6/2010.</li>
+      </ul>
+      <h6>Badinh Secondary and Primary school</h6>
+      <ul>
+        <li>Graduate at 6/2007. Award 3rd in Physic of Hanoi city in grade 9.</li>
+      </ul>
+
+      <div className="h-8 " name="break" />
+      <h3>Communication</h3>
+      <ul>
+        <li>Run events for recruiting new members of BNI as president role.</li>
+        <li>Run event for student association as MC role to promote student social life.</li>
+      </ul>
+      <div className="h-8 " name="break" />
+      <h3>Leadership</h3>
+      <h6>President, Vice president, Secretary, Head of Communication – BNI.</h6>
+      <ul>
+        <li>Play leadership roles in Business Network International (BNI) of an chapter (BNI Power).</li>
+        <li>Get familiar with many business models.</li>
+      </ul>
+      <h6>President at VSA – Vietnamese Student Association in McNeese State University.</h6>
+      <ul>
+        <li>Prepare and run each successful event in school with participate of 250-300 international students.</li>
+        <li>Financial budget and raise fund from the school.</li>
+        <li>Motivate association’s members to volunteer.</li>
+      </ul>
+      <div className="h-8 " name="break" />
+      <h3>References</h3>
+      <h6>McNeese State University</h6>
+      <ul>
+        <li class="list-inline-item">Reference: Dr.Chen</li>
+        <li class="list-inline-item">Email: jschen@mcneese.edu</li>
+      </ul>
+      <div className="h-8 " name="break" />
+      <ul className="list-unstyled list-inline">
+        <li className="list-inline-item text-center">Master Degree Certificates</li>
+        <li class="list-inline-item">
+          <img src="/images/McNeese.jpg" class="w-8/12 m-auto" alt="mcneesecert" />
+        </li>
+      </ul>
+      <div className="h-8 " name="break" />
+      <ul className="list-unstyled list-inline">
+        <li className="list-inline-item text-center">Bachelor Degree Certificates</li>
+        <li class="list-inline-item">
+          <img src="/images/EASB bachelor Cardiff.jpg" class="w-8/12 m-auto" alt="easecert" />
+        </li>
+      </ul>
+      <div className="h-8 " name="break" />
+      <ul className="list-unstyled list-inline">
+        <li className="list-inline-item text-center">London Accounting Certificates</li>
+        <li class="list-inline-item">
+          <img src="/images/LCCI cert.jpg" class="w-8/12 m-auto" alt="londonacc" />
+        </li>
+
+      </ul>
+    </div>
   )
 
 }
